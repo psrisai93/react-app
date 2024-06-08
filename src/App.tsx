43593edf777ff42e817c8f0e75fb8d84c24f6900
Eswara <psrisai93@gmail.com>
2024-06-08 15:28:28 +0530

    passing data via props

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
-import ListGroup from './components/ListGroup';
-function App(){
-  return <div><ListGroup/></div>
+import ListGroup from "./components/ListGroup";
+function App() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  return (
+    <div>
+      <ListGroup items={items} heading="Cities" />
+    </div>
+  );
 }
-export default App
+export default App;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,18 +1,30 @@
-import { MouseEvent } from "react";
+import { MouseEvent, useState } from "react";
+interface Props {
+  items: string[];
+  heading: string;
+}
 
-function ListGroup() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-
-  //event handler
-  const handleClick = (event: MouseEvent) => console.log(event);
-
+function ListGroup({ items, heading }: Props) {
+  //Hook - state hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {items.length === 0 && <p>Nothing</p>}
       <ul className="list-group">
         {items.map((item, index) => (
-          <li className="list-group-item" key={item} onClick={handleClick}>
+          <li
+            //dynamic class assignement(This is basic. There are advanced assignment of dynamic classses)
+            className={
+              selectedIndex == index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
             {item}
           </li>
         ))}
